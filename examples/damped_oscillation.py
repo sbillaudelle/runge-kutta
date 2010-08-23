430c8f29f3b5313a2ecc47efcfa183174fbf4efc
Sebastian Billaudelle <stein@cream-project.org>
2010-08-23 20:42:19 +0200

    Cleanup.

--- examples/damped_oscillation.py
+++ examples/damped_oscillation.py
@@ -26,7 +26,7 @@ ydot = lambda t, x, y: -(R*y + D*x) / m
 xdot = lambda t, x, y: -(m*ydot(t, x, y) + D*x) / R
 
 lv = RK4(xdot, ydot)
-res = lv.solve([0, 1], .01, 2d0)
+res = lv.solve([0, 1], .01, 20)
 
 for i in res:
     print i[0], i[1][0], i[1][1]
